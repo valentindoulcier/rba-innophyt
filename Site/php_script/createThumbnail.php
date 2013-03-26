e2a373cc8b98906cd7fcc6f561a2580e350cd7d0
Valentin DOULCIER <valentin.doulcier@gmail.com>
2013-03-26 17:29:29 +0100

    Revert "[ RBA-INNOPHYT ] Site - Thumbnail"
    This reverts commit 955862262a4ccf2ee93a95b19c312bc014c86a38.

--- Site/php_script/createThumbnail.php
+++ Site/php_script/createThumbnail.php
@@ -8,32 +8,98 @@ define(MEDIA_ARBRE_PATH, $MEDIA_ARBRE_PATH);
 define(THUMBNAIL_ARBRE_PATH, $THUMBNAIL_ARBRE_PATH);
 
 $mosaiqueArray = array();
+$nbMedias = 0;
 
-function parse($mosaiqueArray) {
+function parse($mosaiqueArray, $nbMedias) {
 	
 	$reader = new XMLReader();
 	$reader->open(XML_FILEPATH);
 
 	while($reader->read()) {
-		if($reader->name=='img' && $reader->nodeType == XMLReader::ELEMENT) {
-			array_push($mosaiqueArray, $reader->getAttribute('src'));
+	
+		if ($reader->nodeType == XMLReader::ELEMENT && $reader->name == 'resultat') {
+			$res = array();
+			array_push($res, $reader->getAttribute('id'));
+			
+			while($reader->read()) {
+				if ($reader->nodeType==XMLReader::END_ELEMENT && $reader->name=='resultat') {
+					array_push($res, $medias);
+					array_push($mosaiqueArray, $res);
+					break;
+				}
+				
+				if($reader->name=='nom' && $reader->nodeType == XMLReader::ELEMENT) {
+					if($reader->read()){
+						array_push($res, $reader->value);
+					}
+				}
+				
+				if($reader->name=='type' && $reader->nodeType == XMLReader::ELEMENT) {
+					if($reader->read()){
+						array_push($res, $reader->value);
+					}
+				}
+				
+				if($reader->name=='regimeAlimentaire' && $reader->nodeType == XMLReader::ELEMENT) {
+					if($reader->read()){
+						array_push($res, $reader->value);
+					}
+				}
+				
+				if($reader->name=='informations' && $reader->nodeType == XMLReader::ELEMENT) {
+					if($reader->read()){
+						array_push($res, $reader->value);
+					}
+					$medias = array();
+				}
+				
+				if($reader->name=='media' && $reader->nodeType == XMLReader::ELEMENT) {
+					$nbMedias++;
+					if($reader->read()) {
+						$media = array();
+						if($reader->read()) {
+							if($reader->name=='img' && $reader->nodeType == XMLReader::ELEMENT) {
+								array_push($media, $reader->getAttribute('id'));
+								array_push($media, $reader->getAttribute('src'));
+							}
+							if($reader->read()) {
+								if($reader->read()) {
+									if($reader->name=='legende' && $reader->nodeType == XMLReader::ELEMENT) {
+										if($reader->read()) {
+											array_push($media, $reader->value);
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+				
+				if($reader->name=='media' && $reader->nodeType == XMLReader::END_ELEMENT) {
+					array_push($medias, $media);
+				}
+			}
 		}
 	}
-	
 	$reader->close();
 	
 	return $mosaiqueArray;
 }
 
 
-function create($mosaiqueArray) {
+function create($mosaiqueArray, $nbMedias) {
+	$count = 0;
 	
 	for ($numResultat = 0; $numResultat < count($mosaiqueArray); $numResultat++) {
-		redimensionner_image(MEDIA_ARBRE_PATH . "/" .$mosaiqueArray[$numResultat]);
+		if(count($mosaiqueArray[$numResultat][5]) != 0) {
+			for ($numMedia = 0; $numMedia < count($mosaiqueArray[$numResultat][5]); $numMedia++) {
+				redimensionner_image(MEDIA_ARBRE_PATH . "/" .$mosaiqueArray[$numResultat][5][$numMedia][1], ++$count);
+			}
+		}
 	}
 }
 
-function redimensionner_image($fichier) {
+function redimensionner_image($fichier, $count) {
 		
 	$file = $fichier;
 	# L'emplacement de l'image à redimensionner. L'image peut être de type jpeg, gif ou png
@@ -61,15 +127,16 @@ function redimensionner_image($fichier) {
 
 		if ($size['mime'] == 'image/jpeg') {
 
+			//imagejpeg($img_mini, "../arbres/thumbnail/thumbnail" . $count . ".jpeg"); /*$THUMBNAIL_ARBRE_PATH*/
 			imagejpeg($img_mini, $new_path); /*$THUMBNAIL_ARBRE_PATH*/
 
 		} elseif ($size['mime'] == 'image/png') {
 					
-			imagepng($img_mini, $new_path, 0, PNG_NO_FILTER);
+			imagepng($img_mini, "../arbres/thumbnail/thumbnail" . $count . ".png", 0, PNG_NO_FILTER);
 
 		} elseif ($size['mime'] == 'image/gif') {
 
-			imagegif($img_mini, $new_path, 0);
+			imagegif($img_mini, "../arbres/thumbnail/thumbnail" . $count . ".gif", 0, PNG_NO_FILTER);
 
 		}
 
@@ -92,8 +159,8 @@ function createFolderIfNeeded($fichier) {
 	return $path;
 }
 
-$mosaiqueArray = parse($mosaiqueArray);
+$mosaiqueArray = parse($mosaiqueArray, $nbMedias);
 
-create($mosaiqueArray);
+create($mosaiqueArray, $nbMedias);
 
 ?>
