8cac4cc643a589c0533f4a276da7a871b0301cc4
Valentin DOULCIER <valentin.doulcier@gmail.com>
2013-03-26 17:29:50 +0100

    Revert "[ RBA-INNOPHYT ] Site - Mosaique
    Parseur tout neuf pour la deuxième fois ...
    
    ABORD DE MER**

--- Site/php_script/mosaique.php
+++ Site/php_script/mosaique.php
@@ -9,76 +9,57 @@ define(THUMBNAIL_ARBRE_PATH, $THUMBNAIL_ARBRE_PATH);
 define(IMAGE_PATH, $IMG_PATH);
 define(QUIZZ_URL,$QUIZZ_URL);
 
-function parse($mosaiqueArray, $nbMedias) {
+function parse($mosaiqueArray) {
 	
 	$reader = new XMLReader();
 	$reader->open(XML_FILEPATH);
-
+	$chemins = array();
+	
 	while($reader->read()) {
 	
+		if ($reader->nodeType == XMLReader::ELEMENT && $reader->name == 'reponse') {
+			$reponse = array();
+			array_push($reponse, $reader->getAttribute('id'));
+		}
+			
 		if ($reader->nodeType == XMLReader::ELEMENT && $reader->name == 'resultat') {
-			$res = array();
-			array_push($res, $reader->getAttribute('id'));
+			$resultat = array();
+			$medias = array();
+			array_push($resultat, $reader->getAttribute('id'));
+			
 			
 			while($reader->read()) {
+				
 				if ($reader->nodeType==XMLReader::END_ELEMENT && $reader->name=='resultat') {
-					array_push($res, $medias);
-					array_push($mosaiqueArray, $res);
+					if(count($medias) != 0) {
+						array_push($resultat, $medias);
+						array_push($reponse, $resultat);
+						array_push($mosaiqueArray, $reponse);
+					}
 					break;
 				}
 				
-				if($reader->name=='nom' && $reader->nodeType == XMLReader::ELEMENT) {
-					if($reader->read()){
-						array_push($res, $reader->value);
-					}
-				}
-				
-				if($reader->name=='type' && $reader->nodeType == XMLReader::ELEMENT) {
-					if($reader->read()){
-						array_push($res, $reader->value);
-					}
-				}
-				
-				if($reader->name=='regimeAlimentaire' && $reader->nodeType == XMLReader::ELEMENT) {
-					if($reader->read()){
-						array_push($res, $reader->value);
-					}
-				}
-				
-				if($reader->name=='informations' && $reader->nodeType == XMLReader::ELEMENT) {
-					if($reader->read()){
-						array_push($res, $reader->value);
-					}
-					$medias = array();
-				}
-				
 				if($reader->name=='media' && $reader->nodeType == XMLReader::ELEMENT) {
-					$nbMedias++;
 					if($reader->read()) {
-						$media = array();
 						if($reader->read()) {
 							if($reader->name=='img' && $reader->nodeType == XMLReader::ELEMENT) {
-								array_push($media, $reader->getAttribute('id'));
-								array_push($media, $reader->getAttribute('src'));
-							}
-							if($reader->read()) {
-								if($reader->read()) {
-									if($reader->name=='legende' && $reader->nodeType == XMLReader::ELEMENT) {
-										if($reader->read()) {
-											array_push($media, $reader->value);
-										}
-									}
+								$nomImg = explode("/", $reader->getAttribute('src'));
+								$imgName = $nomImg[count($nomImg) - 1];
+								if(!in_array($imgName, $chemins)) {
+									$media = array();
+									array_push($media, $reader->getAttribute('id'));
+									array_push($media, $reader->getAttribute('src'));
+									array_push($chemins, $imgName);
+									array_push($medias, $media);
 								}
 							}
 						}
 					}
 				}
-				
-				if($reader->name=='media' && $reader->nodeType == XMLReader::END_ELEMENT) {
-					array_push($medias, $media);
-				}
 			}
 		}
+
+		//if ($reader->nodeType == XMLReader::END_ELEMENT && $reader->name == 'reponse') {}
 	}
 	$reader->close();
 	
@@ -86,22 +67,21 @@ function parse($mosaiqueArray, $nbMedias) {
 }
 
 
-function create($mosaiqueArray, $nbMedias) {
-	$count = 0;
+function create($mosaiqueArray) {
 	
 	echo ('<div id="mosaique" class="row">');
 	
 	for ($numResultat = 0; $numResultat < count($mosaiqueArray); $numResultat++) {
-		if(count($mosaiqueArray[$numResultat][5]) != 0) {
-			for ($numMedia = 0; $numMedia < count($mosaiqueArray[$numResultat][5]); $numMedia++) {
+		if(count($mosaiqueArray[$numResultat][1][1]) != 0) {
+			for ($numMedia = 0; $numMedia < count($mosaiqueArray[$numResultat][1][1]); $numMedia++) {
 				?>
-				<div id="<?php echo $mosaiqueArray[$numResultat][5][$numMedia][0] ?>" class="span1 mosaiqueCell" data-idRes="<?php echo $mosaiqueArray[$numResultat][0] ?>" data-nom="<?php echo $mosaiqueArray[$numResultat][1] ?>" data-type="<?php echo $mosaiqueArray[$numResultat][2] ?>" data-regimeAlimentaire="<?php echo $mosaiqueArray[$numResultat][3] ?>" data-informations="<?php echo $mosaiqueArray[$numResultat][4] ?>" data-mediaId="<?php echo $mosaiqueArray[$numResultat][5][$numMedia][0] ?>" data-mediaChemin="<?php echo $mosaiqueArray[$numResultat][5][$numMedia][1] ?>" data-mediaLegende="<?php echo $mosaiqueArray[$numResultat][5][$numMedia][2] ?>">
+				<div id="<?php echo $mosaiqueArray[$numResultat][1][1][$numMedia][0] ?>" class="span1 mosaiqueCell" data-idreponse="<?php echo $mosaiqueArray[$numResultat][0] ?>" data-idresultat="<?php echo $mosaiqueArray[$numResultat][1][O] ?>" data-mediaId="<?php echo $mosaiqueArray[$numResultat][1][1][$numMedia][0] ?>" data-mediaChemin="<?php echo $mosaiqueArray[$numResultat][1][1][$numMedia][1] ?>">
 					<!-- <img src="<?php echo $THUMBNAIL_ARBRE_PATH . "/thumbnail" . $count . $extension; ?>" alt="icone mosaique" class="ico-mosaique"/> -->
 					<!-- <img src="<?php echo THUMBNAIL_ARBRE_PATH . "/medias/" .$mosaiqueArray[$numResultat][5][$numMedia][1]; ?>" alt="icone mosaique" class="ico-mosaique"/> -->
 					<a href="<?php echo QUIZZ_URL ?>">
-						<img src="<?php echo IMAGE_PATH ?>/blank.gif" data-src="<?php echo THUMBNAIL_ARBRE_PATH . "/medias/" .$mosaiqueArray[$numResultat][5][$numMedia][1]; ?>" alt="icone mosaique" class=" lazy ico-mosaique"/>
+						<img src="<?php echo IMAGE_PATH ?>/blank.gif" data-src="<?php echo THUMBNAIL_ARBRE_PATH . "/medias/" .$mosaiqueArray[$numResultat][1][1][$numMedia][1]; ?>" alt="icone mosaique" class=" lazy ico-mosaique"/>
 						<noscript>
-							<img src="<?php echo THUMBNAIL_ARBRE_PATH . "/medias/" .$mosaiqueArray[$numResultat][5][$numMedia][1]; ?>" alt="icone mosaique" class="nolazy ico-mosaique"/>
+							<img src="<?php echo THUMBNAIL_ARBRE_PATH . "/medias/" .$mosaiqueArray[$numResultat][1][1][$numMedia][1]; ?>" alt="icone mosaique" class="nolazy ico-mosaique"/>
 						</noscript>
 					</a>
 				</div>
@@ -114,11 +94,10 @@ function create($mosaiqueArray, $nbMedias) {
 
 function afficher() {
 	$mosaiqueArray = array();
-	$nbMedias = 0;
 
-	$mosaiqueArray = parse($mosaiqueArray, $nbMedias);
+	$mosaiqueArray = parse($mosaiqueArray);
 
-	create($mosaiqueArray, $nbMedias);
+	create($mosaiqueArray);
 
 }
 
